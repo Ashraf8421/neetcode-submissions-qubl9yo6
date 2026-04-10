5f6efbf0e8f038ed69ee10ce4b1ff62ff852add5
Burugala Khaja Ashraf Hussain <101348014+Ashraf8421@users.noreply.github.com>
2026-04-10 10:03:01 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashMap<Integer,Boolean> hm = new HashMap<>();
+       for(int i=0;i<nums.length;i++){
+        if(hm.containsKey(nums[i])){
+            return true;
+        }
+        hm.putIfAbsent(nums[i], true);
+       }
+
+       return false;
+    }
+}
